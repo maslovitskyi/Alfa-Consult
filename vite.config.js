8b1687c3340acc87522433086ec9715316eec2e0
Александр Маслов <A.Maslov@lan.sct.ru>
2025-04-10 08:42:56 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-//f
+
 export default defineConfig({
   base: '/Alfa-Consult/',
   build: {
